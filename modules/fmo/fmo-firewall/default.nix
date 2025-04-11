696cc4ec35dbb2c4be84e559d6e5e962c0d1eb06
Manuel Bluhm <manuel@ssrc.tii.ae>
2025-04-11 11:11:47 +0400

    fix(portforwarding): allow all external interfaces
    Change portforwarding rules for all external interfaces. Needed to allow any random external usb dongle.
    
    Signed-off-by: Manuel Bluhm <manuel@ssrc.tii.ae>

--- modules/fmo/fmo-firewall/default.nix
+++ modules/fmo/fmo-firewall/default.nix
@@ -14,34 +14,25 @@ let
     mkOption
     types
     optionalAttrs
-    concatStringsSep
     concatMapStringsSep
     ;
 
   mkFirewallRules =
     {
-      interface,
       dip,
       sport,
       dport,
       proto,
     }:
     ''
-      iptables -t nat -A PREROUTING -i ${interface} -p ${proto} --dport ${sport} -j DNAT --to-destination ${dip}:${dport}
-      iptables -t nat -A POSTROUTING -o ${interface} -p ${proto} --dport ${sport} -j MASQUERADE
+      iptables -t nat -A PREROUTING -p ${proto} --dport ${sport} -j DNAT --to-destination ${dip}:${dport}
+      iptables -t nat -A POSTROUTING -p ${proto} --dport ${sport} -j MASQUERADE
     '';
 
 in
 {
   options.services.fmo-firewall = {
     enable = mkEnableOption "fmo-firewall";
-
-    externalNics = mkOption {
-      type = types.listOf types.str;
-      description = ''
-        List of external network interfaces with port forwarding rules.
-      '';
-    };
 
     mtu = mkOption {
       type = types.nullOr types.int;
@@ -72,21 +63,17 @@ in
       extraCommands = ''
         ${concatMapStringsSep "\n" (
           rule:
-          (concatMapStringsSep "\n" (
-            nic:
-            mkFirewallRules {
-              interface = nic;
-              inherit (rule) dip;
-              inherit (rule) sport;
-              inherit (rule) dport;
-              inherit (rule) proto;
-            }
-          ) cfg.externalNics)
+          mkFirewallRules {
+            inherit (rule) dip;
+            inherit (rule) sport;
+            inherit (rule) dport;
+            inherit (rule) proto;
+          }
         ) cfg.configuration}
       '';
     };
 
-    # Set MTU
+    # Set all IF MTUs for NetworkManager network interfaces
     environment.etc."NetworkManager/dispatcher.d/99-mtu" = optionalAttrs (cfg.mtu != null) {
       text = ''
         #!/bin/sh
@@ -94,11 +81,7 @@ in
         STATUS="$2"
         case "$STATUS" in
           up)
-            for iface in ${concatStringsSep " " cfg.externalNics}; do
-              if [[ "$IFACE" == "$iface" ]]; then
-                ip link set dev "$IFACE" mtu ${toString cfg.mtu}
-              fi
-            done
+            ip link set dev "$IFACE" mtu ${toString cfg.mtu}
           ;;
         esac
       '';
